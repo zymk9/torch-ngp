22fd194e861f4710a4b42c2664657ea72e5bcb71
yakumo <zymk73@gmail.com>
2023-03-06 18:27:47 +0800

    mask out loss for unlabeled area

--- train_nerf_3dfront.sh
+++ train_nerf_3dfront.sh
@@ -4,10 +4,18 @@ set -x
 set -e
 
 python3 main_nerf_mask.py \
-/data2/jhuangce/torch-ngp/FRONT3D_render/finished/3dfront_0019_00/train \
---workspace ./workspace/3dfront_nerf/3dfront_0019_00 \
---iters 10000 \
+/data/bhuai/temp/3dfront_0110_00 \
+--workspace ./workspace/3dfront_0110_00 \
+--iters 15000 \
 --lr 1e-2 \
---cuda_ray \
---dataset_name 3dfront \
---wandb
+--bound 8 \
+--gpu 5 \
+-O \
+--label_regularization_weight 1.0 \
+--ckpt /data/bhuai/NeRF_RCNN/dependencies/torch-ngp/workspace/3dfront_0110_00/checkpoints/ngp_ep0102.pth \
+--load_model_only \
+--train_mask \
+--num_rays $((4096)) \
+--patch_size 8
+# --test
+# --mask3d /data/bhuai/temp/3dfront_0054_00_filtered.npy \
